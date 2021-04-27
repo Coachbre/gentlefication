d2097525e21367c91e8288d321c731a961e50d9f
Coachbre <brekonan1@gmail.com>
2021-04-27 16:08:59 -0400

    login/register forms complete

---
+++ gentlefication/src/components/gentleApp.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { Route, Redirect } from "react-router-dom"
+import { ApplicationViews } from "./ApplicationViews"
+// import { NavBar } from "./nav/NavBar"
+import { Login } from "./auth/Login"
+import { Register } from "./auth/Register"
+
+export const GentleApp = () => (
+  <>
+    <Route
+      render={() => {
+        if (sessionStorage.getItem("gentle_user")) {
+          return (
+            <>
+              {/* <NavBar /> */}
+              <ApplicationViews />
+            </>
+          )
+          //if user input is recognized, render full page view
+        } else {
+          return <Redirect to="/login" />;
+        }
+        // if not, show 'login' view
+      }}
+    />
+
+    <Route path="/login">
+      <Login />
+    </Route>
+    {/*displays login page view*/}
+
+    <Route path="/register">
+      <Register />
+    </Route>
+    {/*displays register page view*/}
+  </>
+)
