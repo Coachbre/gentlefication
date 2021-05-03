ac2cebe673341c8c3994258ce6cdd3a7788a5774
Coachbre <brekonan1@gmail.com>
2021-05-03 17:05:29 -0400

    added fetch call to council manager

--- gentlefication/src/modules/CouncilManager.js
+++ gentlefication/src/modules/CouncilManager.js
@@ -4,7 +4,13 @@ const remoteURL = "http://localhost:8088"
 // 8088 is the port chosen to host json server
 
 export const getAllCouncil= () => {
-    return fetch(`${remoteURL}/councilMembers`)
+    return fetch(`${remoteURL}/councilMembers/`)
     .then(result => result.json())
     //waits for response, then parses into json data 
+}
+
+export const getCouncilDistricts= () => {
+    return fetch(`${remoteURL}/councilMembers?_expand=district`)
+    .then(result => result.json())
+    //waits for response, then parses into json data
 }
