4bbcefda8edcff90a7c339cb4363242342c1448c
Coachbre <brekonan1@gmail.com>
2021-05-04 22:06:21 -0400

    logout button works

--- gentlefication/src/components/nav/NavBar.js
+++ gentlefication/src/components/nav/NavBar.js
@@ -1,48 +1,56 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 import "./NavBar.css";
 import Logo from "../../images/logo.png";
+
 
 
 //Layout for app navigation bar
 
 export const NavBar = () => {
+
+    const history = useHistory();
+    const handleLogout = () => {
+        sessionStorage.clear();
+        history.push("/login")
+        //handleLogout() clears session storage and redirects to login page
+    }
     return (
-          <nav className="navBar">
+        <nav className="navBar">
 
-                <ul>
-                    <li className="nav-item">
-                        <Link to="/">
-                            <img className="navLogo" src={Logo} alt="logo" />
-                        </Link>
-                    </li>
-
-                    <li className="nav-item">
-                        <Link to="/Events">
-                            EVENTS
+            <ul>
+                <li className="nav-item">
+                    <Link to="/">
+                        <img className="navLogo" src={Logo} alt="logo" />
                     </Link>
-                    </li>
+                </li>
 
-                    <li className="nav-item">
-                        <Link to="/Organizations">
-                            ORGANIZATIONS
+                <li className="nav-item">
+                    <Link to="/Events">
+                        EVENTS
                     </Link>
-                    </li>
+                </li>
 
-                    <li className="nav-item">
-                        <Link to="/Council">
-                            CITY COUNCIL
+                <li className="nav-item">
+                    <Link to="/Organizations">
+                        ORGANIZATIONS
                     </Link>
-                    </li>
+                </li>
 
-                    <li className="log-out">
-                        <Link>
+                <li className="nav-item">
+                    <Link to="/Council">
+                        CITY COUNCIL
+                    </Link>
+                </li>
+
+                <li className="log-out">
+                    <button onClick={handleLogout}>
                         LOG OUT
-                    </Link>
-                    </li>
-                </ul>
-     
-            </nav>
+                    </button>
+                </li>
+            </ul>
+
+        </nav>
 
     )
 }
