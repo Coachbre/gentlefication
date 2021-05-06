1f9d0c2dc065f71bc0ef1631bc85e64aa40ca5bc
Coachbre <brekonan1@gmail.com>
2021-05-06 11:33:16 -0400

    user can now add events to their saved list

--- gentlefication/src/components/events/Events.js
+++ gentlefication/src/components/events/Events.js
@@ -11,6 +11,8 @@ import { SavedEventsPreview } from "./SavedEventsPreview";
 
 export const Events = () => {
 
+    const currentUser = JSON.parse(sessionStorage.getItem("gentle_user"))
+
     const [events, setEvents] = useState([]);
     //'events' is always the current value, 'setEvents' is used to change it, and 'useState' is the initial value
     const getEvents = () => {
@@ -23,34 +25,39 @@ export const Events = () => {
             });
 
     };
-//*********************************** */
+    //*********************************** */
     const [saved, setSaved] = useState({
-        eventId: parseInt(events.id)
-        
+        eventId: parseInt(events.id),
+        userId: currentUser,
+        notes: ""
+
+
     });
 
-    
+
     const getSingleEvent = () => {
         // getSingleEvent() ultimately returns single event object from json array
         return getEventById()
             //fetches json info
-            .then(singleEvent => { 
+            .then(singleEvent => {
                 setSaved(singleEvent)
                 // waits for response then sets 'saved' variable equal to the API data
             });
     };
 
-    const handleAddToList = (id) => {
-
+    const handleAddToList = (eventObj /*taco*/) => {
+      //
         const saved = {
-        
-            eventId: parseInt(id)
+            eventId: parseInt(eventObj.id),
+            userId: currentUser,
+            //logs current users id # as userId
+            notes: ""
         }
         addToSavedList(saved)
-        .then(() => getAllSaved())
+            .then(() => getAllSaved())
     };
 
- //******************************************* */
+    //******************************************* */
 
     useEffect(() => {
         //useEffect() is used to call the getEvents() function
@@ -73,8 +80,8 @@ export const Events = () => {
                                     key={eventObj.id}
                                     //unique key used by react (not required, but good convention)
                                     event={eventObj}
-                                // eventObj (each event in the array) is now equal to 'event' (prop passed into EventCard)
-                                handleAddToList={handleAddToList}
+                                    // eventObj (each event in the array) is now equal to 'event' (prop passed into EventCard)
+                                    handleAddToList={handleAddToList}
                                 />
                             </ul>
 
