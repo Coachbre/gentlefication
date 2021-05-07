200492b5c43c0098b1409b8211b1599511091515
Coachbre <brekonan1@gmail.com>
2021-05-07 00:59:14 -0400

    council member images resized

--- gentlefication/src/components/council/CouncilCard.js
+++ gentlefication/src/components/council/CouncilCard.js
@@ -1,6 +1,7 @@
 //handles layout of each city council member card
 
 import React from 'react';
+import './Council.css'
 
 
 export const CouncilCard = ({council}) => (
@@ -9,11 +10,11 @@ export const CouncilCard = ({council}) => (
     
         <div className="memberHeader">
         <h2 className="memberName">{council.name} - {council.district.name}</h2>
-        <a href={council.url}>Visit Website</a>
+        <button><a href={council.url}>Visit Website</a></button>
         </div>
         <div className="membercontact">
         <h3>CONTACT INFO</h3>
-        <div className="memberPhone">PHONE:{council.phone}</div>
+        <div className="memberPhone">PHONE: {council.phone}</div>
         <div className="memberEmail">EMAIL: {council.email}</div>
         </div>
 
